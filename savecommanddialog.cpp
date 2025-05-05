6e7a33efd5585315a7dcea08d4a176ac2023baad
pengxianhong <290677893@qq.com>
2025-05-05 18:19:50 +0800

    feat(gui): 添加扩展指令对话框
    - 设计并实现了添加扩展指令的对话框界面
    - 添加了指令值和备注的输入框，并设置了相应的样式
    -增加了保存和取消按钮，并设置了按钮样式
    - 优化了对话框的布局，使其更加美观和易于使用

--- savecommanddialog.cpp
+++ savecommanddialog.cpp
@@ -7,12 +7,51 @@
 #include "savecommanddialog.hpp"
 #include "ui_SaveCommandDialog.h"
 
+static void setLineEditStyle(const Ui::SaveCommandDialog *ui) {
+    const auto materialLineEditStyle = R"(
+        QLineEdit {
+            border: 1px solid #757575;
+            border-radius: 4px;
+            padding: 3px;
+            background-color: white;
+            color: #212121;
+            font: 10pt "微软雅黑"
+        }
+
+        QLineEdit:hover {
+            border-color: #6200EE
+        }
+
+        QLineEdit:focus {
+            border-color: #6200EE
+        }
+
+        QLineEdit:disabled {
+            background-color: #E0E0E0;
+            color: #BDBDBD;
+            border-color: #BDBDBD
+        }
+
+        QLineEdit::placeholder {
+            color: #757575
+        }
+    )";
+
+    auto applyStyle = [=](QLineEdit *lineEdit) {
+        lineEdit->setStyleSheet(materialLineEditStyle);
+    };
+
+    applyStyle(ui->commandValueView);
+    applyStyle(ui->remarkValueView);
+}
+
 SaveCommandDialog::SaveCommandDialog(QWidget *parent) : QDialog(parent), ui(new Ui::SaveCommandDialog) {
     ui->setupUi(this);
+
+    setLineEditStyle(ui);
 }
 
 QString SaveCommandDialog::getInputValue() const {
-
 }
 
 SaveCommandDialog::~SaveCommandDialog() {
