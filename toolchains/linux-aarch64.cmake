19265b8f2f9dfcaf5424ea4fdf257571174e1857
pengxianhong <290677893@qq.com>
2025-05-07 17:07:50 +0800

    build(aarch64): 更新编译器和工具链配置
    - 在 GitHub Actions 中添加 gcc-aarch64-linux-gnu 和 libc6-dev-arm64-cross 包
    - 更新 linux-aarch64.cmake 文件:
      - 移除中文注释，使用英文注释  - 优化编译器和路径设置
      - 设置环境变量 QTDIR 代替 CMAKE_PREFIX_PATH

--- toolchains/linux-aarch64.cmake
+++ toolchains/linux-aarch64.cmake
@@ -1,13 +1,13 @@
-# 指定目标系统
+# toolchains/linux-aarch64.cmake
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-# 指定 C 和 C++ 编译器
-set(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
+# Specify the cross-compiler
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
 
-# Specify the root directory for the target system
+# Specify the sysroot directory
 set(CMAKE_SYSROOT /usr/aarch64-linux-gnu)
 
-# 指定 Qt5 的路径
-set(CMAKE_PREFIX_PATH /usr/lib/aarch64-linux-gnu/qt5)
+# Specify the Qt directory for the target system
+set(ENV{QTDIR} /usr/lib/aarch64-linux-gnu/qt5)
