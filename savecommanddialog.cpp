7ce4f233de2265499ee64ea376299f821b569c04
pengxianhong <290677893@qq.com>
2025-05-12 22:13:24 +0800

    feat(savecommanddialog): 使用 QPlainTextEdit 替代 QLineEdit 以支持多行指令输入
    - 将 commandValueView 从 QLineEdit 更改为 QPlainTextEdit
    - 更新样式表以保持一致的外观和感觉- 修改相关函数以适应新的多行输入方式

--- savecommanddialog.cpp
+++ savecommanddialog.cpp
@@ -9,55 +9,15 @@
 
 #include "utils.hpp"
 
-static void setLineEditStyle(const Ui::SaveCommandDialog *ui) {
-  const auto materialLineEditStyle = R"(
-        QLineEdit {
-            border: 1px solid #757575;
-            border-radius: 4px;
-            padding: 3px;
-            background-color: white;
-            color: #212121;
-            font: 10pt "微软雅黑"
-        }
-
-        QLineEdit:hover {
-            border-color: #6200EE
-        }
-
-        QLineEdit:focus {
-            border-color: #6200EE
-        }
-
-        QLineEdit:disabled {
-            background-color: #E0E0E0;
-            color: #BDBDBD;
-            border-color: #BDBDBD
-        }
-
-        QLineEdit::placeholder {
-            color: #757575
-        }
-    )";
-
-  auto applyStyle = [=](QLineEdit *lineEdit) {
-    lineEdit->setStyleSheet(materialLineEditStyle);
-  };
-
-  applyStyle(ui->commandValueView);
-  applyStyle(ui->remarkValueView);
-}
-
 SaveCommandDialog::SaveCommandDialog(QWidget *parent,
                                      const QString &defaultValue,
                                      const QString &defaultRemark)
     : QDialog(parent), ui(new Ui::SaveCommandDialog) {
   ui->setupUi(this);
 
-  setLineEditStyle(ui);
-
   // 如果有初始值，则设置为编辑模式
   if (!defaultValue.isEmpty()) {
-    ui->commandValueView->setText(defaultValue);
+    ui->commandValueView->setPlainText(defaultValue);
     ui->remarkValueView->setText(defaultRemark);
     setWindowTitle("编辑扩展指令");
   }
@@ -69,7 +29,7 @@ SaveCommandDialog::SaveCommandDialog(QWidget *parent,
 }
 
 void SaveCommandDialog::onSaveCommandButtonClicked() {
-  const QString commandValue = ui->commandValueView->text();
+  const QString commandValue = ui->commandValueView->toPlainText();
   if (commandValue.isEmpty()) {
     QMessageBox::warning(this, "提示", "请输入指令值为空");
     return;
@@ -88,7 +48,7 @@ void SaveCommandDialog::onCancelButtonClicked() { close(); }
 Command SaveCommandDialog::getInputValue() const {
   Command command;
   // 返回输入的值，并转换为大写，并添加空格 格式化
-  const auto hex = ui->commandValueView->text().toUpper();
+  const auto hex = ui->commandValueView->toPlainText().toUpper();
   if (hex.contains(" ")) {
     command.setValue(hex);
   } else {
